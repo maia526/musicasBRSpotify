de5579c9260f97979823dd7b020b60b5b767b9e1
Mariana Maia <96018595+maia526@users.noreply.github.com>
2024-07-23 22:17:30 -0300

    Update detectLyricsLanguage.py

--- detectLyricsLanguage.py
+++ detectLyricsLanguage.py
@@ -3,7 +3,7 @@ from lyricsgenius import Genius
 from langdetect import detect
 
 def detectarLingua(nomeMusica, nomeArtista):
-    token = "8aGtIIz8kQbHXnX-AwVlgAv0CK7QbfLjICEm3OjpPOzinOL6OmRNM6kl-qLn65Ww"
+    token = ""
     genius = Genius(token)
     song = genius.search_song(title=nomeMusica, artist=nomeArtista)
     if (song == None):
